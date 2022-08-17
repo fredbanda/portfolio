5c816937d53afebeb1d1902a7cfcf1cc263b49be
Fred Banda <97950605+fredbanda@users.noreply.github.com>
2022-08-17 08:06:47 +0200

    Update client.js

--- src/client.js
+++ src/client.js
@@ -2,12 +2,12 @@ import sanityClinet from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClinet({
-  projectId: "w0m9yqbz",
+  projectId: "w0m9ylk/;kqbz",
   dataset: "production",
   apiVersion: "2022-08-10",
   useCdn: true,
   token:
-    "skzsOaDIkZTrugxkmyd0eONe77G0kEhukM1YmwV4tlKG6gLuIsPUIc2NTTpcf22s33l09wfdI9BmcAQNDx1AZCRiZcYcYXJmVkxBgEikDkPmA3YVdkM0XIruUjA9ndqpQlArvUguZp9CkfrcYIPS5ssS3CPsMnhaLlnJhR1XJfMijmR4M7pp",
+    "skzsOaDIkZTrugxkmyd0eONeklj;kplkp9uliulhiu77G0kEhukM1mcAQNDx1AZCRiZcYcYXJmVkxBgEikDkPmA3YVdkM0XIruUjA9ndqpQlArvUguZp9CkfrcYIPS5ssS3CPsMnhaLlnJhR1XJfMijmR4M7pp",
 });
 
 const builder = imageUrlBuilder(client);
